63282aad815a49d4fdb9cb68b22c0492175e2c30
karma244 <monkeyman7763@gmail.com>
2023-10-20 19:38:32 +0900

    Solved BOJ 11021

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@ use std::io::stdin;
 
 fn main() {
     let mut input_text = String::new();
-    stdin()
-        .read_line(&mut input_text)
-        .unwrap();
-
+    stdin().read_line(&mut input_text).unwrap();
     let x = input_text.trim().parse::<i32>().unwrap();
-    //INTEGER가 아닌 문자열 입력 시 오류 뱉음, 공백 입력 시 오류 뱉음.
+    for i in 1..=x {
+        let mut buffer = String::new();
+        stdin().read_line(&mut buffer).unwrap();
+        let input:Vec<usize> = buffer.split_whitespace().map(|s| s.trim().parse().unwrap()).collect::<Vec<_>>();
+        
+        print!("Case #{i}: {}\n", input[0]+input[1]);
+    }
 }
