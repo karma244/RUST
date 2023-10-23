f9baf904a3c880e90f625d2216b0169fad0e8194
karma244 <monkeyman7763@gmail.com>
2023-10-23 19:14:10 +0900

    Making resources for the ps

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,60 @@
-use std::io::stdin;
+use io::Write;
+use std::{io, str, io::stdin, io::stdout};
 
-//BOJ 2438
-fn main() {
-    let mut buffer = String::new();
-    stdin().read_line(&mut buffer).unwrap();
-    let input = buffer.trim().parse::<i32>().unwrap();
+pub struct Scanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
 
-    for i in 1..=input {
-        for j in 0..i {
-            print!("*");
+impl<R: io::BufRead> Scanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
         }
-        println!();
     }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().unwrap();
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .unwrap();
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+
+//BOJ 2562
+fn main() {
+    let mut scan = Scanner::new(stdin().lock());
+    let mut out = io::BufWriter::new(stdout().lock());
+
+    let mut arr = vec![0; 9];
+
+    for i in 0..9 {
+        let x = scan.token::<i32>();
+        arr[i] = x;
+    }
+
+    let mut max = i32::MIN;
+    let mut idx = 0;
+    //GG
+    for i in 0..9 as usize {
+        if arr[i] > max {
+            max = arr[i];
+            idx = i + 1;
+        }
+    }
+    //GG
+    writeln!(out, "{}\n{}", max, idx).unwrap();
 }
